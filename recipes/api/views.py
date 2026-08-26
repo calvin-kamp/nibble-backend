53bfb613bfdc53d58c41b2d0b230eeda041746be
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-08-26 03:03:09 +0200

    feat(recipes): update recipe views

--- recipes/api/views.py
+++ recipes/api/views.py
@@ -1,19 +1,18 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 
 from recipes.models import Recipe
 
+from .filters import RecipeFilter
 from .serializers import RecipeDetailSerializer, RecipeListSerializer
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     permission_classes = [AllowAny]
-
-    def get_queryset(self):
-        queryset = Recipe.objects.all()
-
-        return queryset
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = RecipeFilter
 
     def get_serializer_class(self):
         if self.action == "list":

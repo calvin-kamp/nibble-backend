caec87381bf83f607c97b628cf2e3a6e25a20968
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-08-26 22:15:12 +0200

    feat(auth): add token blacklist app and simplejwt settings + logout auth route

--- accounts/auth/urls.py
+++ accounts/auth/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
+    TokenBlacklistView,
     TokenRefreshView,
     TokenVerifyView,
 )
@@ -9,6 +10,7 @@ from .views import LoginView, RegisterView
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
+    path("logout/", TokenBlacklistView.as_view(), name="token_blacklist"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]

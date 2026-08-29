45e12be347d42824f0390bfa26a93b3a552b2e02
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-08-29 17:45:37 +0200

    chore: remove django_extensions from installed apps

--- core/settings.py
+++ core/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_filters",
-    "django_extensions",
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
